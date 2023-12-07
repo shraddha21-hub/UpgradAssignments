10b32794b228eecc8a2f575e5f2c5abe3507ce83
shraddha21-hub <kaleshraddha21@gmail.com>
2023-12-07 11:38:21 +0530

    addition of two numbers program

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        //exchange value of variable a & b
+        int a=10;
+        int b=11;
+        int c;
+        c=b;
+        b=a;
+        a=c;
+        System.out.println("value b:"+b +"value a" +a);
+        //addition of 2 numbers
+        int s1=10;
+        int s2=11;
+        int sum=s1+s2;
+        //Subtraction of 2 numbers
+        int sub=s1-s2;
+        System.out.println("Addition of two no :" +sum +"Substraction of two no :" +sub);
+
+    }
+}
