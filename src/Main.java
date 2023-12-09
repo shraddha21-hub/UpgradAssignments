b48dd8a4ddee68113824d251d2a93e0de40bf4f6
shraddha21-hub <kaleshraddha21@gmail.com>
2023-12-09 19:42:22 +0530

    addition of two numbers program

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,23 @@ public class Main {
         int sub=s1-s2;
         System.out.println("Addition of two no :" +sum +"Substraction of two no :" +sub);
 
+        //java code to print number positions at ones,tens,hundreds,thousands
+        int num =  23456;
+        int ones = num%10;
+        System.out.println("numbers in the ones : " +ones);
+        num = num/10;
+        int tens = num%10;
+        System.out.println("numbers in the tens : " +tens);
+        num = num/10;
+        int hundreds = num%10;
+        System.out.println("numbers in the hundreds : " +hundreds);
+        num = num/10;
+        int thousands = num%10;
+        System.out.println("numbers in the thousands : " +thousands);
+        num = num/10;
+        int ten_thousands = num%10;
+        System.out.println("numbers in the ten thousands : " +ten_thousands);
+
+
     }
 }
