5fda2902eab81f3a6e636d8d479797a5ac23a705
shraddha21-hub <kaleshraddha21@gmail.com>
2023-12-30 22:36:59 +0530

    Array addition Programme added

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         //exchange value of variable a & b
         int a=10;
         int b=11;
@@ -17,26 +18,26 @@ public class Main {
         //Subtraction of 2 numbers
         int sub=s1-s2;
         System.out.println("Addition of two no :" +sum +"Substraction of two no :" +sub);
-
-        //java code to print number positions at ones,tens,hundreds,thousands,ten thousands
-        int num =  23456;
+        
+            //java code to print number positions at ones,tens,hundreds,thousands,ten thousands
+        int num =  sc.nextInt();
         int ones = num%10;
-        System.out.println("numbers in the ones : " +ones);
+        System.out.println(ones);
         num = num/10;
         int tens = num%10;
-        System.out.println("numbers in the tens : " +tens);
+        System.out.println(tens);
         num = num/10;
         int hundreds = num%10;
-        System.out.println("numbers in the hundreds : " +hundreds);
+        System.out.println(hundreds);
         num = num/10;
         int thousands = num%10;
-        System.out.println("numbers in the thousands : " +thousands);
+        System.out.println(thousands);
         num = num/10;
         int ten_thousands = num%10;
-        System.out.println("numbers in the ten thousands : " +ten_thousands);
+        System.out.println(ten_thousands);
 
         //swapping two numbers without using third variable
-        Scanner sc = new Scanner(System.in);
+
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
         num1 = num1+num2;
@@ -60,5 +61,17 @@ public class Main {
         float celcius = (float)cel;
         float f = (9*celcius)/5+32;
         System.out.println(f);
+        //java programe to sum number from 100 to 200 which is divisible by 9
+        //write programe to take array elements from user & add it up
+        System.out.println("Please enter number of elemnts you want to provide");
+        int n = sc.nextInt();
+        int [] arr = new arr[n];
+        for(int i=0; i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++) {
+            System.out.println(arr[i]);
+        }
+        }
     }
 }
